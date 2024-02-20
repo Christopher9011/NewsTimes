68528a548e288cfc4c1acd255b2685e0a6257858
Christopher9011 <134900463+Christopher9011@users.noreply.github.com>
2024-02-20 22:39:27 +0900

    Update main.js
    Add render function and style UI

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-let news = [];
+// const API_KEY = `0c328b291853445b8357c74f52a8e78c`;
+let newsList = [];
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://news-times-v1.netlify.app/top-headlines?`
-  );
+  const url = new URL(`https://news-times-v1.netlify.app/top-headlines?`);
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("success", news);
+  newsList = data.articles;
+  render();
+  console.log("success", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+  <div class="col-lg-4">
+    <img
+      class="news-img-size"
+      src=${news.urlToImage}
+      alt="news_image"
+    />
+  </div>
+  <div class="col-lg-8">
+    <h2>${news.title}</h2>
+    <p>${news.description}</p>
+    <div>${news.source.name} * ${news.publishAt}</div>
+  </div>
+</div>`
+    )
+    .join("");
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
