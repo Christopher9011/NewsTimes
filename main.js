0593a16da57f4a6af882832edc38c93b7c609359
Christopher9011 <134900463+Christopher9011@users.noreply.github.com>
2024-02-22 23:44:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
 
 const getLatestNews = async () => {
   const url = new URL(`https://news-times-v1.netlify.app/top-headlines?`);
@@ -6,7 +10,31 @@ const getLatestNews = async () => {
   const data = await response.json();
   newsList = data.articles;
   render();
-  console.log("success", newsList);
+  // console.log("success", newsList);
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  const url = new URL(
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=${category}`
+  );
+
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+};
+
+const searchNews = async () => {
+  const keyword = document.getElementById("search-input").value;
+  const url = new URL(
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=${keyword}`
+  );
+
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
 };
 
 const openNav = () => {
